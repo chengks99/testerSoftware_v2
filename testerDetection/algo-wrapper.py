d0a669f820aa54c3eea1ac8d099e36b2c6e152e4
Seo Juneyoung <122007604+xeojune@users.noreply.github.com>
2024-04-02 08:09:59 +0100

    Update algo-wrapper.py

--- testerDetection/algo-wrapper.py
+++ testerDetection/algo-wrapper.py
@@ -8,8 +8,10 @@ import sys
 import logging
 import pathlib
 import threading
+import serial
 
 from plugin_module import PluginModule
+from final_algo import TesterDetection
 
 scriptPath = pathlib.Path(__file__).parent.resolve()
 sys.path.append(str(scriptPath.parent / 'common'))
@@ -22,8 +24,8 @@ class AlgoWrapper(PluginModule):
         self.id = 'vid{}'.format(args.id)
         self.algo = None
         self.subscribe_channels = [
-            'tester.{}.result'.format(self.id),
-            'tester.{}.alert'.format(self.id),
+            'tester.{}.response'.format(self.id),
+            'tester.{}.alert-response'.format(self.id),
         ]
         self.redis_conn = au.connect_redis_with_args(args)
 
@@ -39,24 +41,34 @@ class AlgoWrapper(PluginModule):
         self.th = threading.Thread(target=self.wrapper)
         self.th.start()
     
+#def read_from_usb(self, port='/dev/ttyUSB0/', baudrate=9600, timeout=1):
+       # with serial.Serial(port, baudrate, timeout=timeout) as ser:
+            #.sleep(2)
+            #data = ser.readline().decode('utf-8').rstrip()
+            #return data
+    
     def wrapper (self):
         ''' wrapper to start algo code in thread'''
         while True:
             if self.algo is None:
-                '''
-                    FIXME: call algo class and fit into self.algo
-                    self.algo = XXXX
-                '''
-                pass
+
+                # self.algo = TesterDetection('/Users/juneyoungseo/Documents/Panasonic/test_videos/2023-12-29 08-08-11 SDU CT Tester.mp4', self.redis_conn, self.id)
+                self.algo = TesterDetection("/dev/video0", self.redis_conn, self.id)
+                #self.algo = TesterDetection(read_from_usb, self.redis_conn, self.id)))
+
             if self.th_quit.is_set():
-                '''
-                    FIXME: call close to the algo class
-                '''
+
+                self.algo.close()
                 break
-    
+
+    # def start_algo(self):
+    #     self.algo = TesterDetection('/Users/juneyoungseo/Documents/Panasonic/test_videos/2023-12-26 10-36-47-ex2 SDU CT Tester.mp4', self.redis_conn, self.id)
+
+
     def process_redis_msg (self, ch, msg):
         ''' process redis msg '''
         if ch in self.subscribe_channels:
+            print (ch, msg)
             if ch == 'tester.{}.response'.format(self.id):
                 self._process_response_msg(msg)
             if ch == 'tester.{}.alert-response'.format(self.id):
@@ -72,48 +84,64 @@ class AlgoWrapper(PluginModule):
         elif _stage == 'testScreen':
             self._response_test_screen(msg)
 
-    def _process_alert_msg (self, msg):
+    def _process_alert_response_msg (self, msg):
         ''' process alert response msg '''
         _stage = msg.get('stage', 'error')
         if _stage == 'alert-reset':
-            self._respone_alert_reset(msg)
+            self._response_alert_reset(msg)
         else:
             self._response_alert(msg)
-    
-    '''
-        FIXME: fill in operation for all response msg
-    '''
+
     def _response_init (self, msg):
         ''' process init stage '''
         _status = msg.get('status', 'failed')
+
         if _status == 'success':
-            #FIXME call self.algo to process capture image
-            #FIXME if self.algo success to start image capture, publish redis msg
-            self.redis_conn.publish(
-                'tester.{}.result'.format(self.id),
-                json2str({
-                    'stage': 'beginCapture',
-                    'status': 'success'
-                })
-            )
+            self.algo.load_configuration()
+        else:
+            logging.error("Initialization Process Failed...")
+
 
     def _response_begin_capture (self, msg):
-        pass
+        _status = msg.get('status', 'failed')
+
+        if _status == 'success':
+            self.algo.capture_test_screen()
+        else:
+            logging.error("Capturing Process Failed...")
 
     def _response_test_screen (self, msg):
-        pass
+        _status = msg.get('status', 'failed')
 
-    def _respone_alert_reset (self, msg):
-        pass
+        if _status == 'success':
+            self.algo.start_mask_compare()
+        else:
+            logging.error("Test Screen Process Failed...")
+
+    def _response_alert_reset (self, msg):
+        _status = msg.get('status', 'failed')
+        if _status == 'success':
+            self.algo.set_alert_stage(msg.get('stage', 'alert-reset'), status=True)
+        else:
+            logging.error('Alert reset failed ...')
 
     def _response_alert (self, msg):
-        pass
-    
+        _status = msg.get('status', 'failed')
+        if _status == 'success':
+            self.algo.set_alert_stage(msg.get('stage', 'alert-msg'), status=False)
+        else:
+            logging.error('Alert setting failed ...')
+
+    # def close_algo(self):
+    #     self.algo.close()
     def algo_close (self):
         ''' close the module '''
         self.th_quit.set()
 
 if __name__ == "__main__":
+    scriptPath = pathlib.Path(__file__).parent.resolve()
+    sys.path.append(str(scriptPath.parent / 'backendServer/adaptor'))
+
     from adaptor import add_common_adaptor_args
     parser = au.init_parser('Algo Wrapper')
     add_common_adaptor_args(
